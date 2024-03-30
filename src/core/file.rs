2f61c6ae66f0aa6b3d4df63b873177e35e7b5684
Mitry <isorrowno@gmail.com>
2024-03-30 18:18:41 -0300

    ?

--- src/core/file.rs
+++ src/core/file.rs
@@ -1,9 +1,7 @@
 use super::Range;
 use actix_web::web::Bytes;
-use futures::io::ReadExact;
 use futures::{AsyncReadExt, FutureExt, Stream};
 use mongodb::{gridfs::FilesCollectionDocument, GridFsDownloadStream};
-use std::rc::Rc;
 use std::{
     io::Error,
     pin::Pin,
@@ -16,8 +14,6 @@ pub struct FileStream {
     pub range: Range,
     pub file_name: String,
     chunk_size: u64,
-    poll: Option<ReadExact<'static, GridFsDownloadStream>>,
-    buffer: Rc<Vec<u8>>,
 }
 
 impl FileStream {
@@ -27,8 +23,6 @@ impl FileStream {
             range,
             file_name: file.filename.unwrap_or(String::from("no_name")),
             chunk_size: file.chunk_size_bytes as u64,
-            poll: None,
-            buffer: Rc::new(Vec::with_capacity(file.chunk_size_bytes as usize)),
         }
     }
 }
@@ -42,16 +36,13 @@ impl Stream for FileStream {
         let mut buffer = vec![0; this.chunk_size as usize];
         let mut poll = this.stream.read(&mut buffer);
 
-        // TODO: dont forget to do something with blocking
-        loop {
-            match poll.poll_unpin(cx) {
-                Poll::Pending => continue,
-                Poll::Ready(Err(e)) => return Poll::Ready(Some(Err(e))),
-                Poll::Ready(Ok(0)) => return Poll::Ready(None),
-                Poll::Ready(Ok(read)) => {
-                    buffer.truncate(read);
-                    return Poll::Ready(Some(Ok(Bytes::from(buffer))));
-                }
+        match poll.poll_unpin(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Err(e)) => return Poll::Ready(Some(Err(e))),
+            Poll::Ready(Ok(0)) => return Poll::Ready(None),
+            Poll::Ready(Ok(read)) => {
+                buffer.truncate(read);
+                return Poll::Ready(Some(Ok(Bytes::from(buffer))));
             }
         }
     }
